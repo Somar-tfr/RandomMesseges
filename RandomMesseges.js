afa9fe220e320ea565b2fa3d87cd7386c313c8e0
Somar-tfr <soumar_taifour@hotmail.com>
2022-11-30 12:51:51 +0100

    - changing random calculations, adding takler, logging title of program, logging random silly joke!

--- RandomMesseges.js
+++ RandomMesseges.js
@@ -16,24 +16,39 @@ let questions = ["chicken cross the street", "Roman empire fall", "elephant ride
 let answers = ["there was a hole on the ground", "princes were fighting about power", "riding a horse is expensive", "a taxi was on fire!", "it was too late to do the opposite", "it was Boring"];
 
 // random calculation of words
-let personInd = Math.floor(Math.random() * 5);
+let personInd = Math.floor(Math.random() * persons.length);
 let person = persons[personInd];
 
-let placeInd = Math.floor(Math.random() * 6);
+let talkerInd = Math.floor(Math.random() * person.length)
+let talker = person[talkerInd];
+
+let placeInd = Math.floor(Math.random() * places.length);
 let place = places[placeInd];
 
-let verbInd = Math.floor(Math.random() * 3);
+let verbInd = Math.floor(Math.random() * verbs.length);
 let verb = verbs[verbInd];
 
-let drinkInd = Math.floor(Math.random() * 5);
+let drinkInd = Math.floor(Math.random() * drinks.length);
 let drink = drinks[drinkInd];
 
-let questionInd = Math.floor(Math.random() * 6);
+let questionInd = Math.floor(Math.random() * questions.length);
 let question = questions[questionInd];
 
-let answerInd = Math.floor(Math.random() * 6);
+let answerInd = Math.floor(Math.random() * answers.length);
 let answer = answers[answerInd];
 
+
+
 //print final result
-//testing results
-console.log(person, place, verb, drink, question, answer);
+console.log("Silly Random jokes generator: \n+++++++++++++++++++++++++++++");
+let messege = "";
+for (let i = 0; i < 3; i ++){
+    if (person[i].startsWith("a") || person[i].startsWith("e") || person[i].startsWith("i") || person[i].startsWith("o") || person[i].startsWith("u")){
+        messege += "an " + person[i] + " ";
+    }else{
+        messege += "a " + person[i] + " ";
+    }
+}
+messege += "and " + verb + " " + drink + ", the " + talker + " asked:\n -\"Why did the " + question + "?\" \n -\"Because " + answer + "!\"";
+//printing results
+console.log(messege);
