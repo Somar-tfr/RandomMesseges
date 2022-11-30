d20813b6c5dc1579c182c442e42ae431a319f1c8
Somar-tfr <soumar_taifour@hotmail.com>
2022-11-30 12:02:19 +0100

    planing layout and adding lists of messeges to RandomMesseges.js file

--- RandomMesseges.js
+++ RandomMesseges.js
@@ -1,5 +1,17 @@
+//random silly jokes generator
+
 // lists of messeges
+//list of persons 
+let persons = [["duck", "chicken","dog"], ["teacher", "worker", "old man"], ["woman", "man", "child"], ["pizza", "chair", "phone"], ["artist", "kangaroo", "penguin"]];
+
+let places = ["bar", "school", "restaurant", "shopping center", "Beachbar", "swimming pool" ];
+
+let verb = ["ordered", "tried", "took"];
+//why did
+let question = ["chicken cross the street", "Roman empire fall", "elephant ride a Volkswagen", "Firefighter use the bus", "dragon disappear", "dinosaur go extinct"];
+
+//because 
+let answers = ["there was a hole on the ground", "princes were fighting about power", "riding a horse is expensive", "a taxi was on fire!", "it was too late to do the opposite", "it was Boring"];
 
 // random calculation
-
 //print final result
