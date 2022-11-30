e09b00d4ada0908c383abfb9401257a6bf9155d5
Somar-tfr <soumar_taifour@hotmail.com>
2022-11-30 12:24:15 +0100

    Storing message components ,calculating random words.

--- RandomMesseges.js
+++ RandomMesseges.js
@@ -6,12 +6,34 @@ let persons = [["duck", "chicken","dog"], ["teacher", "worker", "old man"], ["wo
 
 let places = ["bar", "school", "restaurant", "shopping center", "Beachbar", "swimming pool" ];
 
-let verb = ["ordered", "tried", "took"];
+let verbs = ["ordered", "tried", "took"];
+
+let drinks = ["beer", "whiskey", "coffe", "some milk", "some Tea"]
 //why did
-let question = ["chicken cross the street", "Roman empire fall", "elephant ride a Volkswagen", "Firefighter use the bus", "dragon disappear", "dinosaur go extinct"];
+let questions = ["chicken cross the street", "Roman empire fall", "elephant ride a Volkswagen", "Firefighter use the bus", "dragon disappear", "dinosaur go extinct"];
 
 //because 
 let answers = ["there was a hole on the ground", "princes were fighting about power", "riding a horse is expensive", "a taxi was on fire!", "it was too late to do the opposite", "it was Boring"];
 
-// random calculation
+// random calculation of words
+let personInd = Math.floor(Math.random() * 5);
+let person = persons[personInd];
+
+let placeInd = Math.floor(Math.random() * 6);
+let place = places[placeInd];
+
+let verbInd = Math.floor(Math.random() * 3);
+let verb = verbs[verbInd];
+
+let drinkInd = Math.floor(Math.random() * 5);
+let drink = drinks[drinkInd];
+
+let questionInd = Math.floor(Math.random() * 6);
+let question = questions[questionInd];
+
+let answerInd = Math.floor(Math.random() * 6);
+let answer = answers[answerInd];
+
 //print final result
+//testing results
+console.log(person, place, verb, drink, question, answer);
